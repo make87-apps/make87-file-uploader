aef494579fe6205e4e7f65bb4c08864e6508e295
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-02-18 12:32:14 -0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Provide Endpoint Template for Python
+# make87 Storage File Uploader
+
+This is a simple file uploader that allows you to upload files to the make87 storage.
+Every file is stored under `{system_path}/{relative_file_path}`. The `relative_file_path` is provided in the received
+request.
--- app/main.py
+++ app/main.py
@@ -1,24 +1,44 @@
-from datetime import datetime, timezone
+import logging
+from pathlib import Path
 
 from make87_messages.core.header_pb2 import Header
-from make87_messages.text.text_plain_pb2 import PlainText
 import make87 as m87
+from make87_messages.file.simple_file_pb2 import RelativePathFile
+from make87_messages.primitive.bool_pb2 import Bool
 
 
 def main():
     m87.initialize()
-    endpoint = m87.get_provider(name="EXAMPLE_ENDPOINT", requester_message_type=PlainText,
-                                provider_message_type=PlainText)
+    endpoint = m87.get_provider(
+        name="RELATIVE_PATH_FILE", requester_message_type=RelativePathFile, provider_message_type=Bool
+    )
 
-    def callback(message: PlainText) -> PlainText:
-        received_dt = datetime.now(tz=timezone.utc)
-        publish_dt = message.header.timestamp.ToDatetime().replace(tzinfo=timezone.utc)
-        print(
-            f"Received message '{message.body}'. Sent at {publish_dt}. Received at {received_dt}. Took {(received_dt - publish_dt).total_seconds()} seconds."
+    storage_path = m87.get_system_storage_path()
+
+    def callback(message: RelativePathFile) -> Bool:
+        relative_path = Path(message.path)
+        if relative_path.is_absolute():
+            logging.error("Path is not relative. Returning Success: False.")
+            return Bool(
+                header=m87.header_from_message(Header, message=message, append_entity_path="success"),
+                value=False,
+            )
+
+        file_path = storage_path / message.path
+        try:
+            file_path.write_bytes(message.data)
+            logging.info(f"File written to {file_path}.")
+        except Exception as e:
+            logging.error(f"Error writing file: {e}. Returning Success: False.")
+            return Bool(
+                header=m87.header_from_message(Header, message=message, append_entity_path="success"),
+                value=False,
+            )
+
+        return Bool(
+            header=m87.header_from_message(Header, message=message, append_entity_path="success"),
+            value=True,
         )
-
-        return PlainText(header=m87.header_from_message(Header, message=message, append_entity_path="response"),
-                         body=message.body[::-1])
 
     endpoint.provide(callback)
     m87.loop()
