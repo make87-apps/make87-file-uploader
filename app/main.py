9126a2e00103fa7d1ca9b13493a3b6c8070d9f28
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-04-03 16:58:31 -0700

    Updated dependencies, manifests, general code things and cleanup

--- app/main.py
+++ app/main.py
@@ -2,26 +2,26 @@ import logging
 from pathlib import Path
 
 from make87_messages.core.header_pb2 import Header
-import make87 as m87
+import make87
 from make87_messages.file.simple_file_pb2 import RelativePathFile
 from make87_messages.primitive.bool_pb2 import Bool
 
 
 def main():
-    m87.initialize()
-    endpoint = m87.get_provider(
+    make87.initialize()
+    endpoint = make87.get_provider(
         name="RELATIVE_PATH_FILE", requester_message_type=RelativePathFile, provider_message_type=Bool
     )
 
-    path_prefix = m87.get_config_value("PATH_PREFIX", default="")
-    storage_path = m87.get_system_storage_path() / Path(path_prefix)
+    path_prefix = make87.get_config_value("PATH_PREFIX", default="")
+    storage_path = make87.get_system_storage_path() / Path(path_prefix)
 
     def callback(message: RelativePathFile) -> Bool:
         relative_path = Path(message.path)
         if relative_path.is_absolute():
             logging.error("Path is not relative. Returning Success: False.")
             return Bool(
-                header=m87.header_from_message(Header, message=message, append_entity_path="success"),
+                header=make87.header_from_message(Header, message=message, append_entity_path="success"),
                 value=False,
             )
 
@@ -32,17 +32,17 @@ def main():
         except Exception as e:
             logging.error(f"Error writing file: {e}. Returning Success: False.")
             return Bool(
-                header=m87.header_from_message(Header, message=message, append_entity_path="success"),
+                header=make87.header_from_message(Header, message=message, append_entity_path="success"),
                 value=False,
             )
 
         return Bool(
-            header=m87.header_from_message(Header, message=message, append_entity_path="success"),
+            header=make87.header_from_message(Header, message=message, append_entity_path="success"),
             value=True,
         )
 
     endpoint.provide(callback)
-    m87.loop()
+    make87.loop()
 
 
 if __name__ == "__main__":
